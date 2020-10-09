0d6c412dceb784214ac2f520d82350904052537c
Samathy Barratt <samathy@sbarratt.co.uk>
2020-10-09 15:16:27 +0100

    Add tests for getting labels and ID

--- source/test/test_vertex.cpp
+++ source/test/test_vertex.cpp
@@ -58,3 +58,34 @@ TEST_CASE ("test_remove_non_existant_label")
 
     REQUIRE_FALSE(v.removeLabel("Schmesting"));
 }
+
+TEST_CASE("test_get_labels")
+{
+
+    vertex v = vertex();
+
+    v.addLabel("Testing");
+
+    REQUIRE(v.getLabels()[0] == "Testing");
+}
+
+TEST_CASE("test_get_labels_as_string")
+{
+
+    vertex v = vertex();
+
+    v.addLabel("Testing");
+    v.addLabel("Testing2");
+    
+    std::cout << v.getLabelsAsString();
+    REQUIRE(v.getLabelsAsString() == "[Testing, Testing2, ]");
+}
+
+TEST_CASE("test_get_id")
+{
+    vertex v = vertex(10);
+
+    REQUIRE(v.getID() == 10);
+}
+
+
