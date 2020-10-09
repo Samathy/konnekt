2e76820407c3b33701f3ddcc19b95341c1585708
Samathy Barratt <samathy@sbarratt.co.uk>
2020-10-09 18:07:10 +0100

    Add implementation for edge method getOther

--- source/edge.h
+++ source/edge.h
@@ -24,6 +24,20 @@ public:
    */
   const std::shared_ptr<vertex> getV2() { return this->v2; }
 
+  /** Which ever vertex you pass it, you get the other one.
+   * Or we throw
+   */
+  const std::shared_ptr<vertex> getOther(std::shared_ptr<vertex> v) {
+      if ( v == this->v1 )
+          return this->v2;
+      else if ( v ==this->v2) 
+          return this->v1;
+
+      throw "This edge is not attached to the given vertex";
+
+  }
+
+
 private:
   std::shared_ptr<vertex> v1;
   std::shared_ptr<vertex> v2;
--- source/test/test_edge.cpp
+++ source/test/test_edge.cpp
@@ -1,16 +1,18 @@
 #include "catch2/catch.hpp"
 
+#include <memory>
+
 #include "../vertex.h"
 #include "../edge.h"
 
 TEST_CASE("test_get_other_vertex")
 {
 
-    vertex v = vertex();
-    vertex v1 = vertex();
+    std::shared_ptr<vertex> v = std::make_shared<vertex>();
+    std::shared_ptr<vertex> v1 = std::make_shared<vertex>();
 
-    v.addLabel("Testing");
-    v1.addLabel("Testing2");
+    v->addLabel("Testing");
+    v1->addLabel("Testing2");
 
     edge e = edge(v,v1);
 
