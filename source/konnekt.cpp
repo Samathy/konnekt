6fb4b0a07574bd7384e1964320271a4dc4fbf6e3
Samathy Barratt <samathy@sbarratt.co.uk>
2020-10-09 16:44:47 +0100

    One can now specify a graph to ingest

--- source/konnekt.cpp
+++ source/konnekt.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include "ingest.h"
 
-int main()
+int main(int argc, char ** argv)
 {
-    std::unique_ptr<graph>  g;
-    g = ingest_json("example1.json");
+    std::unique_ptr<graph> g;
+    if ( argc >= 1 )
+    {
+        std::cout << "Ingesting graph from: " << argv[1] << std::endl;
+        g = ingest_json(argv[1]);
+    }
+    else
+        std::cout << "Ingesting graph from exmaple1.json";
+        g = ingest_json("example1.json");
+
+    std::cout << g->getDot();
 }
